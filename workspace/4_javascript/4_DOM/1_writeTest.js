c5004445eb5f9cefaac342df1307e8637fc6024b
rhg4656 <llloe@naver.com>
2024-05-02 21:47:48 +0900

    5월 2일 DOM 시작

---
+++ workspace/4_javascript/4_DOM/1_writeTest.js
@@ -0,0 +1,26 @@
+let dom = `<form action="#">`+
+`<fieldset>`+
+`<legend>1번 범주</legend>` +
+`<input type="text">` +
+`</fieldset>` +
+`<fieldset>` +
+`<legend>2번 범주</legend>` +
+`<input type="text">` +
+`</fieldset>` +
+`</form>`;
+
+// document.write(dom);
+
+// 백틱으로 편하게 dom 을 그릴 수 있다.
+let dom2 = `<form action="#">
+<fieldset>
+    <legend>1번 범주</legend>
+    <input type="text">
+</fieldset>
+<fieldset>
+    <legend>2번 범주</legend>
+    <input type="text">
+</fieldset>
+</form>`
+
+document.write(dom2)
